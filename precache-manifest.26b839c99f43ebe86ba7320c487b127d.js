486dd723dfb5ca4d2f1639354ad203400882250c
Bigshmow <dstewart88@gmail.com>
2019-11-04 10:19:32 -0800

    Updates

---
+++ precache-manifest.26b839c99f43ebe86ba7320c487b127d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f9011c895dc486929186cff19cab97f",
+    "url": "/Clicky-state-game/index.html"
+  },
+  {
+    "revision": "c2eec3acbb2396479c7b",
+    "url": "/Clicky-state-game/static/css/main.fe4b24fc.chunk.css"
+  },
+  {
+    "revision": "a65eac30dd3e5136fe78",
+    "url": "/Clicky-state-game/static/js/2.ca20fa8c.chunk.js"
+  },
+  {
+    "revision": "c2eec3acbb2396479c7b",
+    "url": "/Clicky-state-game/static/js/main.85666aae.chunk.js"
+  },
+  {
+    "revision": "aa0cadc19f18726fcbc4",
+    "url": "/Clicky-state-game/static/js/runtime-main.d5d505a6.js"
+  }
+]);
